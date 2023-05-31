3c759f56e4a6e645774eed55cea33d2deffbba51
Zsuzsi Vegvari <zsuzsanna.vegvari@gmail.com>
2023-05-31 15:15:00 +0200

    finalize the patchgenerator

--- src/main/java/com/codecool/marsexploration/logic/PatchGenerator.java
+++ src/main/java/com/codecool/marsexploration/logic/PatchGenerator.java
@@ -13,16 +13,24 @@ public class PatchGenerator {
     private TerrainElementType terrainElementType;
     private int numberOfTerrainElements;
     private MarsMap marsMap;
+    List<Coordinate> potentialPositions;
     Random random;
 
     public PatchGenerator(TerrainElementType terrainElementType, int numberOfTerrainElements, MarsMap marsMap) {
         this.terrainElementType = terrainElementType;
         this.numberOfTerrainElements = numberOfTerrainElements;
         this.marsMap = marsMap;
+        this.potentialPositions = new ArrayList<>();
     }
 
-    public void generateMountain() {
+    public void generatePatch() {
         putElementOnMap(pickFirstPosition());
+        potentialPositions = potentialNextPositions(pickFirstPosition());
+        for (int i = 1; i < numberOfTerrainElements; i++) {
+            Coordinate actualPositionToAdd = randomCoordinate(potentialPositions);
+            putElementOnMap(actualPositionToAdd);
+            potentialPositions.remove(actualPositionToAdd);
+        }
     }
 
     public Coordinate pickFirstPosition() {
@@ -36,7 +44,6 @@ public class PatchGenerator {
     }
 
     public List<Coordinate> potentialNextPositions(Coordinate previousCoordinate) {
-        List<Coordinate> potentialPositions = new ArrayList<>();
         for (int dx = -1; dx < 1; dx++) {
             for (int dy = -1; dy < 1; dy++) {
                 if (checkNeighbourIsEmpty(previousCoordinate.x(), previousCoordinate.y(), dx, dy) &&
@@ -44,30 +51,29 @@ public class PatchGenerator {
                         isWithInBound(previousCoordinate.x(), previousCoordinate.y(), dx, dy)) {
                     potentialPositions.add(new Coordinate(previousCoordinate.x() + dx, previousCoordinate.y() + dy));
                 }
-
             }
         }
-            return potentialPositions;
+        return potentialPositions;
     }
 
-        private boolean checkNeighbourIsEmpty(int x, int y, int dx, int dy){
-            return marsMap.getTerrainElements()[x + dx][y + dy].getType() == TerrainElementType.EMPTY;
+    private boolean checkNeighbourIsEmpty(int x, int y, int dx, int dy) {
+        return marsMap.getTerrainElements()[x + dx][y + dy].getType() == TerrainElementType.EMPTY;
 
-        }
+    }
 
-        private boolean checkNeighbourType (int x, int y, int dx, int dy){
-            return marsMap.getTerrainElements()[x + dx][y + dy].getType() == terrainElementType;
+    private boolean checkNeighbourType(int x, int y, int dx, int dy) {
+        return marsMap.getTerrainElements()[x + dx][y + dy].getType() == terrainElementType;
 
-        }
+    }
 
 
     private boolean isWithInBound(int x, int y, int dx, int dy) {
-          return x + dx >= 0 &&  y + dy >= 0 && x + dx <= marsMap.getWidth() && y + dy <= marsMap.getHeight();
+        return x + dx >= 0 && y + dy >= 0 && x + dx <= marsMap.getWidth() && y + dy <= marsMap.getHeight();
 
     }
 
     public Coordinate randomCoordinate(List<Coordinate> potentialNextPositions) {
-        return null;
+        return potentialNextPositions.get(random.nextInt(potentialNextPositions.size()));
     }
 
 
