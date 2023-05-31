cc8ad110d9314afe4db488215da008ff9a70472b
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-05-31 22:28:42 +0200

    test randomCoordinate method

---
+++ src/test/java/com/codecool/marsexploration/logic/PatchGeneratorTest.java
@@ -0,0 +1,28 @@
+package com.codecool.marsexploration.logic;
+
+import com.codecool.marsexploration.data.Coordinate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatchGeneratorTest {
+
+    @Test
+    public void testRandomCoordinate() {
+        PatchGenerator patchGenerator = new PatchGenerator(null, 0, null);
+        List<Coordinate> potentialNextPositions = new ArrayList<>();
+        potentialNextPositions.add(new Coordinate(1, 2));
+        potentialNextPositions.add(new Coordinate(3, 4));
+        potentialNextPositions.add(new Coordinate(5, 6));
+
+        Coordinate result = patchGenerator.randomCoordinate(potentialNextPositions);
+        System.out.println(result);
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(potentialNextPositions.contains(result));
+    }
+
+}
