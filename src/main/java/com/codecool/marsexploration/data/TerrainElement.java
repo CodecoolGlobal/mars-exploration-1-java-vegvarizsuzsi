f5293c7a09a7faf22c187c4b7986366502aa1649
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-05-31 09:58:58 +0200

    implement TerrainElement

--- src/main/java/com/codecool/marsexploration/data/TerrainElement.java
+++ src/main/java/com/codecool/marsexploration/data/TerrainElement.java
@@ -1,5 +1,15 @@
 package com.codecool.marsexploration.data;
 
-public abstract class TerrainElement {
+public class TerrainElement {
+
+    private TerrainElementType type;
+
+    public TerrainElement(TerrainElementType type) {
+        this.type = type;
+    }
+
+    public TerrainElementType getType() {
+        return type;
+    }
 
 }
