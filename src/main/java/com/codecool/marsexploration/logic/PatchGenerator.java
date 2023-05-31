656b7c01c10ac598eabad183c989bedd8509fa86
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-05-31 11:28:43 +0200

    implement PatchGenerator constructor and add base method names

--- src/main/java/com/codecool/marsexploration/logic/PatchGenerator.java
+++ src/main/java/com/codecool/marsexploration/logic/PatchGenerator.java
@@ -1,4 +1,35 @@
 package com.codecool.marsexploration.logic;
 
+import com.codecool.marsexploration.data.Coordinate;
+import com.codecool.marsexploration.data.MarsMap;
+import com.codecool.marsexploration.data.TerrainElementType;
+
+import java.util.List;
+
 public class PatchGenerator {
+    private TerrainElementType terrainElementType;
+    private int numberOfTerrainElements;
+    private MarsMap marsMap;
+
+    public PatchGenerator(TerrainElementType terrainElementType, int numberOfTerrainElements, MarsMap marsMap) {
+        this.terrainElementType = terrainElementType;
+        this.numberOfTerrainElements = numberOfTerrainElements;
+        this.marsMap = marsMap;
+    }
+    public void generateMountain(){}  //
+    public Coordinate pickFirstPosition(){return null;}
+
+    public void putElementOnMap(Coordinate coordinate){
+        marsMap.putElementOnMap(coordinate, terrainElementType);
+    }
+
+    public List<Coordinate> potentialNextPositions(){
+        return null;}
+
+    public Coordinate randomCoordinate(List<Coordinate> potentialNextPositions){
+        return null;
+    }
+
+
+
 }
